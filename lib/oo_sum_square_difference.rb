5ea1e740e9084ac115af226b38ee3b88e40d995a
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-08-16 09:11:40 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
+
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(num)
+    @num = num
+  end
+
+  def difference
+    sum_of_squares = 0
+    sum = 0
+    (1..@num).each do |int|
+      sum += int
+      sum_of_squares += int * int
+    end
+    square_of_sum = sum * sum
+    square_of_sum - sum_of_squares
+  end
+end
+
+p SumSquareDifference.new(10).difference
